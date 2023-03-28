42eadf7774af87170f28b44a5d25525ea97a6d9a
Khondoker Khademul Bari <khademul.bari@v2.ltd>
2023-03-28 22:02:55 +0600

    updated code

---
+++ basic/db/db_user.py
@@ -0,0 +1,24 @@
+from db.hash import Hash
+from sqlalchemy.orm.session import Session
+from schemas import UserBase
+from db.models import DbUser
+
+
+def create_user(db: Session, request: UserBase):
+  new_user = DbUser(
+    username = request.username,
+    email = request.email,
+    password = Hash.bcrypt(request.password)
+  )
+  db.add(new_user)
+  db.commit()
+  db.refresh(new_user)
+  return new_user
+
+
+def get_all_users(db:Session):
+  return db.query(DbUser).all()
+
+
+def get_user_by_id(db:Session,id:int):
+  return db.query(DbUser).filter(DbUser.id == id).first()
---
+++ basic/router/user.py
@@ -0,0 +1,34 @@
+from schemas import UserBase, UserDisplay
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
+from typing import List
+from db.database import get_db
+from db import db_user
+
+
+router = APIRouter(
+  prefix='/user',
+  tags=['user']
+)
+
+# Create user
+@router.post('/', response_model=UserDisplay)
+def create_user(request: UserBase, db: Session = Depends(get_db)):
+  return db_user.create_user(db, request)
+
+# Read All users
+@router.get('/', response_model=List[UserDisplay])
+def get_all_user(db: Session = Depends(get_db)):
+  return db_user.get_all_users(db)
+
+# get user by id
+@router.get('/{id}', response_model=UserDisplay)
+def get_user_by_id(id:int, db: Session = Depends(get_db)):
+  return db_user.get_user_by_id(db,id)
+
+
+
+
+# Update user
+
+# Delete user
